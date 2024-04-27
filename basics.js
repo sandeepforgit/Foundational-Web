6e287043b12209ef0c6b58d1aa2b02f727177d3e
sandeepforgit <sandeepleela9@gmail.com>
2024-04-27 16:02:01 +0530

    practised interpolation in javascript

--- basics.js
+++ basics.js
@@ -1,5 +1,13 @@
-var x = "sandeep";
-x = "leela";
+string interpolation method
+let firstName = "Sandeep";
+let lastName = "Chodapuneedi";
+let maritalStatus = "Single";
+let country = "India";
+let age = 20;
+console.log(`My name is ${firstName} ${lastName}`);
+console.log(`I'm ${maritalStatus}`);
+console.log(`I'm from ${country}`);
+console.log(`I'm ${age} years old`);
 
-console.log(x);
- 
+
+
