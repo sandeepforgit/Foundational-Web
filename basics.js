746015ac61a3a67c7027d82e575628e58b902ea4
sandeepforgit <sandeepleela9@gmail.com>
2024-04-27 16:04:59 +0530

    practised some String methods

--- basics.js
+++ basics.js
@@ -1,13 +1,14 @@
-string interpolation method
-let firstName = "Sandeep";
-let lastName = "Chodapuneedi";
-let maritalStatus = "Single";
-let country = "India";
-let age = 20;
-console.log(`My name is ${firstName} ${lastName}`);
-console.log(`I'm ${maritalStatus}`);
-console.log(`I'm from ${country}`);
-console.log(`I'm ${age} years old`);
+// string interpolation method
+// let firstName = "Sandeep";
+// let lastName = "Chodapuneedi";
+// let maritalStatus = "Single";
+// let country = "India";
+// let age = 20;
+// console.log(`My name is ${firstName} ${lastName}`);
+// console.log(`I'm ${maritalStatus}`);
+// console.log(`I'm from ${country}`);
+// console.log(`I'm ${age} years old`);
 
-
+let s = "IaM SaNdeEp";
+console.log(s.toUpperCase());
 
