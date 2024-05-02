1ef707296a061d64f35771fb4644b6cff3097a48
sandeepforgit <sandeepleela9@gmail.com>
2024-05-02 20:20:33 +0530

    practised array methods

--- basics.js
+++ basics.js
@@ -9,6 +9,12 @@
 // console.log(`I'm from ${country}`);
 // console.log(`I'm ${age} years old`);
 
-let s = "IaM SaNdeEp";
-console.log(s.toUpperCase());
+// let s = "IaM, SaN, deEp, java, script";
+// // console.log(s.toUpperCase());
+// // console.log(s.includes("s"));
+// const arr = s.split(",");
+// console.log(arr);
 
+let company = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon';
+const arr = company.split(',');
+console.log(arr);
