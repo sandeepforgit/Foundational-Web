a76753806f9f8424422b6887b56abfe98a17bde8
sandeepforgit <sandeepleela9@gmail.com>
2024-06-09 22:47:24 +0530

    done conditional rendering in todoApp

--- react/React_Projects/TodoApp/src/Components/list.jsx
+++ react/React_Projects/TodoApp/src/Components/list.jsx
@@ -6,18 +6,21 @@ function List() {
     const todos = useSelector((state) => state.todos.todos)
     const dispatch = useDispatch()
   return (
-    <div class="grid  outline outline-2 m-10 mx-44 min-h-5">
+    <>
+    {todos.length > 0 && (<div id="list" class="grid outline outline-2 m-10 mx-44 min-h-5">
         {todos.map((todo)=>(
           <>
           <div class="flex gap-5 w-80 p-2">
             <h1 class="flex justify-start" key={todo.id}>*{todo.text}</h1>
-            <button class="self-end outline outline-1 rounded"
-            onClick={()=>dispatch(removeTodo(todo.id))}
-            >Delete</button>
+              <button class="self-end outline outline-1 rounded"
+              onClick={()=>dispatch(removeTodo(todo.id))}
+              >🗑️</button>
           </div>
           </>
           ))}
     </div>
+    )}
+    </>
   )
 }
 
