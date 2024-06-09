8a116a5cfc191ce5d7837ef30c1f5b6e7f70502b
sandeepforgit <sandeepleela9@gmail.com>
2024-06-09 23:09:51 +0530

    added some styling to todoApp

--- react/React_Projects/TodoApp/src/Components/list.jsx
+++ react/React_Projects/TodoApp/src/Components/list.jsx
@@ -7,12 +7,14 @@ function List() {
     const dispatch = useDispatch()
   return (
     <>
-    {todos.length > 0 && (<div id="list" class="grid outline outline-2 m-10 mx-44 min-h-5">
+    {todos.length > 0 && (
+      <div id="list" class="grid justify-center  m-10 mx-44 min-h-5">
         {todos.map((todo)=>(
           <>
-          <div class="flex gap-5 w-80 p-2">
-            <h1 class="flex justify-start" key={todo.id}>*{todo.text}</h1>
-              <button class="self-end outline outline-1 rounded"
+          <div class="flex items-center gap-5 rounded m-2 w-96 p-2 bg-zinc-700 transition ease-in-out hover:-translate-y-1 hover:scale-110">
+            👉
+            <h1 class="p-1 w-80 text-zinc-400" key={todo.id}>{todo.text}</h1>
+              <button class=" outline outline-1 rounded transition ease-in-out hover:-translate-y-1 hover:scale-90"
               onClick={()=>dispatch(removeTodo(todo.id))}
               >🗑️</button>
           </div>
