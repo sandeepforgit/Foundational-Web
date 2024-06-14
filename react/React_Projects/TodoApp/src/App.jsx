aee1ad379a149a1ccfc3c64dfbed2ebaeedb698e
sandeepforgit <sandeepleela9@gmail.com>
2024-06-14 14:55:37 +0530

    done some changes

--- react/React_Projects/TodoApp/src/App.jsx
+++ react/React_Projects/TodoApp/src/App.jsx
@@ -13,11 +13,15 @@ function App() {
       <div class="flex gap-5 ">
         <input
         class="placeholder:text-zinc-500 h-7 w-52 rounded outline outline-1 hover:outline-offset-2 bg-zinc-400 outline-gray-400 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300"
+        value = {inp}
         onChange={(e) => setInp(e.target.value)}
         placeholder='Enter Task'/>
         <button 
         class=" w-20 h-7 text-zinc-400 font-extralight outline outline-gray-400 rounded transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110  duration-300"
-        onClick={() => dispatch(addTodo(inp))}
+        onClick={() => (
+          dispatch(addTodo(inp)),
+          setInp("")
+        )}
         >
         Add Task
         </button>
